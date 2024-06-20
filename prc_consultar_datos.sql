5d51b76dfa9aecf80be736e815f01949ca52c5ee
Alexhecu <alexander.hernandez@softcio.co>
2024-06-20 18:43:53 -0500

    primer comiit en el repo local

--- prc_consultar_datos.sql
+++ prc_consultar_datos.sql
@@ -1,9 +1,10 @@
 declare
-    v_dato1    number;
+    v_dato1    varchar2(4000);
     v_dato2    varchar2(4000);
+	v_dato3    varchar2(4000);
 begin
-    select 1, '2'
-      into v_dato1, v_dato2
+    select 'A', '2','3'
+      into v_dato1, v_dato2,v_dato3
       from dual;
 
     dbms_output.put_line('v_dato1: '|| v_dato1);
