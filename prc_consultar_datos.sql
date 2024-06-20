3618ea07eef04f14896ea81e794ac0b19c9acdef
Antonio Molina <antonio.molina@softcio.co>
2024-06-20 18:12:34 -0500

    feat: adiciona logica de consulta

---
+++ prc_consultar_datos.sql
@@ -0,0 +1,12 @@
+declare
+    v_dato1    number;
+    v_dato2    number;
+begin
+    select 1, 2
+      into v_dato1, v_dato2
+      from dual;
+
+    dbms_output.put_line('v_dato1: '|| v_dato1);
+    dbms_output.put_line('v_dato2: '|| v_dato2);
+end;
+/
